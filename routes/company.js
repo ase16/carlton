830beb8a716806c658a7acb6224e3cb20f6dbe5e
Oliver Leumann <oliver.leumann@gmail.com>
2016-05-01 19:42:20 +0200

    Struggling with CORS, trial and error fix #6

--- routes/company.js
+++ routes/company.js
@@ -3,16 +3,37 @@
 // Third-party modules
 var router = require('express').Router();
 var stormpath = require('express-stormpath');
+var config = require('config');
 var winston = require('winston');
 winston.level = 'info';
 winston.add(winston.transports.File, { filename: 'carltonWinston.log', prettyPrint: true, depth: 20 });
 
+// Express modules
+var cors = require('cors');												// --> https://github.com/expressjs/cors
+
 // Custom modules
 var terms = require('./company/terms');
 
+// Enable CORS
+var corsOrigin = config.get('cors-origin');								// We need to copy the loaded cors properties to a new object, to avoid "read only property" errors
+var corsOptions = {
+	origin: corsOrigin,
+	methods: ['HEAD', 'GET', 'POST', 'PUT', 'DELETE'],
+	credentials: true
+};
+
+console.log("company::corsOptions = ", corsOptions);
+
+router.all('/*', cors(corsOptions), function(req, res, next) {
+	console.log('Entered /company space (1)');
+	winston.info('Entered /company space (1)');
+	next();
+});
+
 // Nod define all routes that are handled after "/company" (maybe we still need this "stormpath.loginRequired,")
-router.all('/*', stormpath.groupsRequired(['companies']), function(req, res, next) {
-	winston.info('Entered /company space');
+router.all('/*', cors(corsOptions), stormpath.groupsRequired(['companies']), function(req, res, next) {
+	console.log('Entered /company space (2)');
+	winston.info('Entered /company space (2)');
 	next();
 });
 
